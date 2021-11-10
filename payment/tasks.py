b19ece00656c5dc6dab753f4f065c0ee741f9088
Leo <ulysses00c@outlook.com>
2021-11-10 14:47:55 +0800

    save

---
+++ payment/tasks.py
@@ -0,0 +1,26 @@
+from io import BytesIO
+from celery import task
+# import weasyprint
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+from orders.models import Order
+
+
+@task
+def payment_completed(order_id):
+    """to send an e-mail when an order was created"""
+    order = Order.objects.get(id=order_id)
+    subject = f'My Shop - EE Invoice no. {order.id}'
+    message = 'Please, find attached the invoice.'
+    email = EmailMessage(subject, message, 'admin@myshop.com', [order.email])
+    # generate PDF
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    out = BytesIO()
+    # stylesheets = [weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+    # weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
+    #attach PDF file
+    email.attach(f'order_{order.id}.pdf',
+                 out.getvalue(),
+                 'application/pdf')
+    email.send()
